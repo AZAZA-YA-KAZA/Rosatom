abf1000ff1a5eadb48e6687cd9b7728314aeb42a
Aswertti <var.yerkhova@gmail.com>
2024-11-05 23:48:08 +0500

    Доработка чатов, логики

--- chat/forms.py
+++ chat/forms.py
@@ -3,4 +3,4 @@ from django.db.models import CharField
 
 
 class ChatForm(forms.Form):
-    text = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'style': 'width: 95%;'}))
+    text = forms.CharField(max_length=1000, widget=forms.TextInput(attrs={'style': 'width: 95%;'}))
